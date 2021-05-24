160d19e036428e78020651ff198b908ac931b885
Seiichi Ariga <seiichi.ariga@gmail.com>
2021-05-24 16:56:14 +0900

    サンプルとして

---
+++ src/private/config-example.nim
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 Seiichi Ariga <seiichi.ariga@gmail.com>
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+
+# Change file name to "config.nim"
+
+const
+  CODE_STATS_API_KEY* = "YOUR_API_KEY"
