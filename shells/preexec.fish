05322e7e0f87dda985f4b9fc94e2d671a90145dc
Seiichi Ariga <seiichi.ariga@gmail.com>
2021-05-25 02:39:21 +0900

    Fish設定の変更

--- shells/preexec.fish
+++ shells/preexec.fish
@@ -1,5 +1,16 @@
 # Seiichi Ariga <seiichi.ariga@gmail.com>
 
+## Fish shellの設定
+## ~/.config/fish/config.fish
+## に追加するfunction
+
 function cs_pulse_preexec --on-event fish_preexec
-  echo "Called : $argv"
+
+## 入力したコマンドの長さ(newlineまで含む)
+  set xp (echo $argv | wc -m)
+
+## exitを送信するとfishが終わらなくなる(?)
+## TODO もっと良い方法
+  if grep -q exit $argv
+    ~/bin/codestats "Terminal(Fish)" $xp 2>/dev/null &
 end
