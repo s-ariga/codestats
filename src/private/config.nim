58e80cc26b26c13128afa9a74935cd9528b58b2c
Seiichi Ariga <seiichi.ariga@gmail.com>
2021-05-24 17:12:37 +0900

    ファイル掃除

--- src/private/config.nim
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2021 Seiichi Ariga <seiichi.ariga@gmail.com>
-# 
-# This software is released under the MIT License.
-# https://opensource.org/licenses/MIT
-
-const
-  CODE_STATS_API_KEY* = "SFMyNTY.Y3kxaGNtbG5ZUT09IyNNVE01TXpnPQ.E03ZJ1FjaG3p_N1qTVzj8Q6JBm-4Ix4pHWrO11iTGm0"
-  VERSION* = "0.1.0"
